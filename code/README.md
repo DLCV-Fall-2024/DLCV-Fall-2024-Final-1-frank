bd8509b805acb49063d19973b0e664be10565281
lycge20923 <lawrence0999@gmail.com>
2024-12-13 08:35:45 +0000

    feat: adjust structure of the contents

--- code/README.md
+++
@@ -1,7 +0,0 @@
-# Preview Dataset 
-
-* Conduct the following code
-
-    ```
-    python3 code/partial_data_viewer.py
-    ```
--- supplement/data_download.py
+++ supplement/data_download.py
@@ -3,11 +3,14 @@ import argparse, json, os
 from PIL import Image
 from tqdm import tqdm
 
+MAX_NUM = 6884
+
 def _parser_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--output_dataset_directory', type=str, help='The folder for storing the datasets', default="dataset")
     parser.add_argument('--dataset_types', type=str, nargs='+', help="the purpose list for the datasets", default=["train", "val", "test"])
-    parser.add_argument('--max_dataset_num', type=int, help="Since the total dataset is too large, so we instead search for the first several data", default=100)
+    parser.add_argument('--max_dataset_num', type=int, help="Since the total dataset is too large, so we instead search for the first several data. \
+        You could still download the whole dataset if set the value to be -1", default=10)
     return parser.parse_args()
 
 def main():
@@ -29,22 +32,26 @@ def main():
         # load dataset, but only load some data
         # streaming = True: would not load the whole dataset
         dataset = load_dataset("ntudlcv/dlcv_2024_final1", split=dataset_type, streaming=True)
-        subset_dataset = list(dataset.take(args.max_dataset_num))
         
         # load the subset of the dataset
-        for item in subset_dataset:
-            id_, conversation, image = item["id"], item["conversations"], item["image"]
-            
-            # insert annotation
-            data = {"id":id_, "conversations":conversation}
-            annotation.append(data)
-            
-            # save image
-            img_dir = os.path.join(output_dir, "images")
-            os.makedirs(img_dir, exist_ok=True)
-            img_path = os.path.join(img_dir, f"{id_}.png")
-            image.save(img_path)
-            
+        dataset_iter = iter(dataset)
+        args.max_dataset_num = args.max_dataset_num if args.max_dataset_num != -1 else MAX_NUM
+        for _ in range(args.max_dataset_num):
+            try:
+                item = next(dataset_iter)
+                id_, conversation, image = item["id"], item["conversations"], item["image"]
+                # insert annotation
+                data = {"id":id_, "conversations":conversation}
+                annotation.append(data)
+                
+                # save image
+                img_dir = os.path.join(output_dir, "images")
+                os.makedirs(img_dir, exist_ok=True)
+                img_path = os.path.join(img_dir, f"{id_}.png")
+                image.save(img_path)
+            except StopIteration:
+                print("End of dataset")
+                break
         # save annotation
         annotation_path = os.path.join(output_dir, "annotation.json")
         with open(annotation_path, 'w') as f:
